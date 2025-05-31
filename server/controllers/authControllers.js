41c3ef63189b387d78bc0275631dd5f8fb4341b2
blytheb <builtbyblythe@gmail.com>
2025-05-30 23:13:20 -1000

    initial commit "

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,42 @@
+const User = require('../models/user')
+
+const test = (req, res) => {
+    res.json('test is working')
+}
+
+//trycatchblock
+const registerUser = async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        //check if name was entered
+        if (!name){
+            return res.json({
+                error: 'Name is required'
+            })
+        }
+        //check if password is good
+        if (!password || password.length < 6){
+            return res.json({
+                error: "Password is required and should be at lesat "
+            })
+        }
+        //check email
+        const exist = await User.findOne({email});
+        if (exist){
+            return res.json({
+                error: 'Email is taken already'
+            })
+        }
+        const user = await User.create({
+            name, email, password
+        })
+        return res.json(user)
+    } catch(err) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    test,
+    registerUser,
+}
